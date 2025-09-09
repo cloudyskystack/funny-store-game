3a4af4993feff10b1fb13548c5d325fcae76b72e
cloudyskystack <scratchingcats.sh@gmail.com>
2025-09-09 11:14:35 +0000

    test

--- test_files/csv_test.cpp
+++ test_files/csv_test.cpp
@@ -6,14 +6,20 @@
 using namespace std;
 
 int main () {
-    ofstream data_file("data.csv");
-    data_file << "test_product,55,32";
-    data_file.close();
-    
     ifstream read_file("data.csv");
     string item_data;
     while (getline(read_file, item_data)) {
-        cout << item_data << endl;
+        stringstream ss(item_data);
+        string item_name;
+        int item_price, item_quantity;
+        char delimiter;
+
+        getline(ss, item_name, ',');
+        ss >> item_price >> delimiter >> item_quantity;
+
+        cout << "Item: " << item_name 
+             << ", Price: " << item_price 
+             << ", Quantity: " << item_quantity << endl;
     }
     read_file.close();
 }
