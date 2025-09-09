bfe563bade41e4e82753f0ca6db2f43a53834812
cloudyskystack <scratchingcats.sh@gmail.com>
2025-09-09 10:13:18 +0000

    edit

--- test_files/csv_test.cpp
+++ test_files/csv_test.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 using namespace std;
 
 int main () {
-    ofstream datafile("data.csv");
-    datafile << "test_product,55,32";
-    datafile.close();
+    ofstream data_file("data.csv");
+    data_file << "test_product,55,32";
+    data_file.close();
+
+    string item_data
+    ifstream read_file("data.csv");
+    while (getline(read_file, item_data)) {
+        cout << item_data;
+    }
+    item_data.close();
 }
